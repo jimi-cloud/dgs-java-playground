895313da7d1ac86eae9f090cf8961ed039e44b31
jimi <jimnorth82@icloud.com>
2023-08-31 15:19:59 -0400

    Add title filterˆ

--- src/main/java/com/jimicloud/dgsjavaplayground/datafetchers/ShowsDataFetcher.java
+++ src/main/java/com/jimicloud/dgsjavaplayground/datafetchers/ShowsDataFetcher.java
@@ -2,8 +2,11 @@ package com.jimicloud.dgsjavaplayground.datafetchers;
 
 import com.jimicloud.dgsjavaplayground.models.Show;
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @DgsComponent
 public class ShowsDataFetcher {
@@ -15,11 +18,16 @@ public class ShowsDataFetcher {
             new Show("4", "Belwo Deck", 2012)
     );
 
+    @DgsQuery
     public Show show(Integer id) {
         return shows.stream().filter(show -> show.id().equals(id.toString())).findFirst().orElse(null);
     }
 
-    public List<Show> shows() {
-        return shows;
+    @DgsQuery
+    public List<Show> shows(@InputArgument String titleFilter) {
+        if (titleFilter == null) {
+            return shows;
+        }
+        return shows.stream().filter(show -> show.title().contains(titleFilter)).toList();
     }
 }
